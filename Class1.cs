dbf76681bd97149b5c1caed313a25b71e24da81f
das44 <das44@DESKTOP-P6B7SJC>
2022-07-11 18:39:02 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Class1
+    {
+        public static double triangleSquare(double ab, double bc, double ac)
+        {
+            if (ab < 0 || bc < 0 || ac < 0)
+            {
+                throw new ArgumentException($"Ошибка: Сторона не может быть меньше 0\n Не надо ломать мою программу(");
+            }
+            if (ab > (bc + ac) || bc > (ab + ac) || ac > (bc + ab))
+            {
+                throw new ArgumentException($"Ошибка: Сторона не может быть больше суммы других сторон\n p.s Я сам не знал");
+            }
+            double p = (ab + bc + ac) / 2;
+            double Square = Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac));
+            return Square;
+        }
+
+        public static double circleSquare(double radius)
+        {
+            if (radius < 0)
+            {
+                Console.WriteLine("Тревога: радиус круга не должен быть отрицательным...");
+            }
+            double Square = Math.PI * radius;
+            return Square;
+        }
+    }
+}
